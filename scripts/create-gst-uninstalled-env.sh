ee5c5e4cb8e2d319a4a7a102e2988909b0a1b864
Petros Kataras <petroskataras@gmail.com>
2016-10-22 15:02:02 +0300

    Add gettext as dependency. Fix #2

--- scripts/create-gst-uninstalled-env.sh
+++ scripts/create-gst-uninstalled-env.sh
@@ -92,7 +92,7 @@ install_gst_dependencies_for_distro () {
 	echo "==========================================================================================="
 	if [ "$DISTRO" = "Debian" ] || [ "$DISTRO" = "Ubuntu" ]; then
 		sudo apt-get update
-		sudo apt-get -y install bison flex git autopoint libtool autoconf liborc-0.4-dev libglib2.0-dev yasm
+		sudo apt-get -y install bison flex git autopoint libtool autoconf liborc-0.4-dev libglib2.0-dev yasm gettext
 
 		# we install standard packages to pull dependencies. Its a bit rough but
 		# but works consistently across debian based distros.
